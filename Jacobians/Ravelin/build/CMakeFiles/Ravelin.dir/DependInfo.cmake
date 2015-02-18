56baf0ed2b37d54da463942486a4bb742b374655
Shankar <shanks.k@gmail.com>
2015-02-18 15:09:56 -0500

    added debug checks to jacobian update

---
+++ Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/AAngled.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/AAngled.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/AAnglef.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/AAnglef.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/FixedJointd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/FixedJointd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/FixedJointf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/FixedJointf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Jointd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Jointd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Jointf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Jointf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/LinAlgd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/LinAlgd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/LinAlgf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/LinAlgf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Matrix2d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Matrix2d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Matrix2f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Matrix2f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Matrix3d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Matrix3d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Matrix3f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Matrix3f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/MatrixNd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/MatrixNd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/MatrixNf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/MatrixNf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/MovingTransform3d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/MovingTransform3d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/MovingTransform3f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/MovingTransform3f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Origin2d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Origin2d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Origin2f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Origin2f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Origin3d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Origin3d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Origin3f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Origin3f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Pose2d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Pose2d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Pose2f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Pose2f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Pose3d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Pose3d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Pose3f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Pose3f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/PrismaticJointd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/PrismaticJointd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/PrismaticJointf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/PrismaticJointf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Quatd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Quatd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Quatf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Quatf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/RevoluteJointd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/RevoluteJointd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/RevoluteJointf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/RevoluteJointf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SForced.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SForced.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SForcef.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SForcef.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SMomentumd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SMomentumd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SMomentumf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SMomentumf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SVector6d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SVector6d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SVector6f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SVector6f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SVelocityd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SVelocityd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SVelocityf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SVelocityf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SharedMatrixNd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SharedMatrixNd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SharedMatrixNf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SharedMatrixNf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SharedVectorNd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SharedVectorNd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SharedVectorNf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SharedVectorNf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SparseMatrixNd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SparseMatrixNd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SparseMatrixNf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SparseMatrixNf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SparseVectorNd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SparseVectorNd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SparseVectorNf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SparseVectorNf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SpatialABInertiad.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SpatialABInertiad.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SpatialABInertiaf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SpatialABInertiaf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SpatialRBInertiad.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SpatialRBInertiad.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SpatialRBInertiaf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SpatialRBInertiaf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SphericalJointd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SphericalJointd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/SphericalJointf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/SphericalJointf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Transform2d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Transform2d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Transform2f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Transform2f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Transform3d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Transform3d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Transform3f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Transform3f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/UniversalJointd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/UniversalJointd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/UniversalJointf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/UniversalJointf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Vector2d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Vector2d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Vector2f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Vector2f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Vector3d.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Vector3d.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/Vector3f.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/Vector3f.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/VectorNd.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/VectorNd.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/VectorNf.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/VectorNf.cpp.o"
+  "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/src/cblas.cpp" "/home/shankar/CSCI6525/gazebo_course/Jacobians/Ravelin/build/CMakeFiles/Ravelin.dir/src/cblas.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Jacobians/build/CMakeFiles/JacobiansPlanar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/JacobiansPlanar.dir/JacobiansPlanar.cpp.o"
+  "libJacobiansPlanar.pdb"
+  "libJacobiansPlanar.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/JacobiansPlanar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
