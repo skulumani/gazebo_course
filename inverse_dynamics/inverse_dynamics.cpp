c9b49354165e95beddab112e71d985826b830f23
Shankar <shanks.k@gmail.com>
2015-04-04 16:51:57 -0400

    inverse dynamics code has some errors

--- inverse_dynamics/inverse_dynamics.cpp
+++ inverse_dynamics/inverse_dynamics.cpp
@@ -13,7 +13,7 @@ public: void CalcInvDyn(double theta1, double theta2, double dtheta1, double dth
   // TODO: put "C" code output from Mathematica here
   // tau1 = ...
   // tau2 = ...
-  tau1 = ((m1+m2)*l1^2+m2*l2^2+2*m2*l1*l2*cos(theta2)) * ddtheta1_des + (m2*l2^2 + m2*l1*l2*cos(theta2))*ddtheta2_des - (m2*l1*l2*(2*dtheta1+dtheta2)*sin(theta2))*dtheta2 + g*((m1+m2)*l1*sin(theta1)+m2*l2*sin(theta1+theta2));
+  tau1 = ((m1+m2)*l1 ^ 2+m2*l2^2+2*m2*l1*l2*cos(theta2)) * ddtheta1_des + (m2*l2^2 + m2*l1*l2*cos(theta2))*ddtheta2_des - (m2*l1*l2*(2*dtheta1+dtheta2)*sin(theta2))*dtheta2 + g*((m1+m2)*l1*sin(theta1)+m2*l2*sin(theta1+theta2));
   tau2 = (m2*l2^2 + m2*l1*l2*cos(theta2))*ddtheta1_des + m2*l2^2*ddtheta2_des + m2*l1*l2*dtheta1*sin(theta2)*dtheta1 + m2*l2*g*sin(theta1+theta2);
   }
 
